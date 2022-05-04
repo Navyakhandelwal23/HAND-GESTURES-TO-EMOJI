dfbc27305fc62e912b8094070bff00d0025e43b8
Navyakhandelwal23 <83491933+Navyakhandelwal23@users.noreply.github.com>
2022-05-04 17:50:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+Webcam.set({
+    width: 350,
+    height:300,
+    image_format:'png',
+    png_quality: 90
+});
+
+prediction1= ""
+prediction2=""
+
+camera= document.getElementById("camera")
+Webcam.attach('#camera')
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id="captured_image"src= "'+data_uri+'">'
+
+    });
+}
+
+console.log("ml5 version",ml5.version)
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/21A1CkFV2/model.json",modelloaded)
+function modelloaded(){
+    console.log("modelisloaded")
+}
+
+function speak(){
+    var synth= window.speechSynthesis
+    speakdata1= "first prediction is "+prediction1
+    speakdata2= "and second prediction is"+prediction2
+    var utterthis= new SpeechSynthesisUtterance(speakdata1+speakdata2)
+    synth.speak(utterthis)
+}
+
+function check(){
+    img= document.getElementById('captured_image')
+    classifier.classify(img,gotresult)
+
+}
+
+function gotresult(error, results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        document.getElementById("result_emotion_name").innerHTML= results[0].label
+        document.getElementById("result_emotion_name2").innerHTML= results[1].label
+        prediction1= results[0].label
+        prediction2= results[1].label
+        speak()
+        if(results[0].label=="Victory"){
+            document.getElementById("update_emoji").innerHTML="&#9996;"
+        }
+        if(results[0].label=="best"){
+            document.getElementById("update_emoji").innerHTML="&#128077;"
+        }
+        if(results[0].label=="Amazimg"){
+            document.getElementById("update_emoji").innerHTML="&#128076;"
+        }
+        
+        if(results[1].label=="Victory"){
+            document.getElementById("update_emoji2").innerHTML="&#9996;"
+        }
+        if(results[1].label=="best"){
+            document.getElementById("update_emoji2").innerHTML="&#128077;"
+        }
+        if(results[1].label=="Amazimg"){
+            document.getElementById("update_emoji2").innerHTML="&#128076;"
+        }
+    }
+}
